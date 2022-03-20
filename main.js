aaddfab8bc9ee4107b4d9a4fd85ea0ac954b6373
anjana29 <80512586+anjana29@users.noreply.github.com>
2022-03-20 17:16:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+video=createCapture(VIDEO);
+video.size(400,400);
+video.position(10,50);
+canvas=createCanvas(400,400);
+canvas.position(430,130);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Intialized");
+}
+function draw(){
+    background("#5196e3");
+}
+function gotPoses(results,error){
+if(error){
+    console.error(error);
+}
+if(results.length>0){
+console.log(results);
+console.log("rightwrist_x = "+results[0].pose.rightWrist.x+"rightwrist_y = "+results[0].pose.rightWrist.y);
+console.log("leftwrist_x = "+results[0].pose.leftWrist.x+"leftwrist_y = "+results[0].pose.leftWrist.y);
+}
+}
